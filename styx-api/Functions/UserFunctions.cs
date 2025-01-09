d5d41a3edae983a6b016dd78411d819be8bead68
Rohan Hangal <rohanhangal@gmail.com>
2025-01-08 21:46:21 -0800

    test for vercel build

--- styx-api/Functions/UserFunctions.cs
+++ styx-api/Functions/UserFunctions.cs
@@ -423,7 +423,7 @@ namespace Styx.Api.Functions
                 return req.CreateResponse(HttpStatusCode.InternalServerError);
             }
 
-            // 7. Return success response
+            // 7. Return success response for uploading photo
             var response = req.CreateResponse(HttpStatusCode.OK);
             var responseBody = new
             {
