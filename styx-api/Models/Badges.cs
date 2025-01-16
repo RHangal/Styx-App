8cf6f8869bc340823b67df10b190c6267bc375f3
Rohan Hangal <rohanhangal@gmail.com>
2025-01-16 15:47:27 -0800

    badges fixed for int value

--- styx-api/Models/Badges.cs
+++ styx-api/Models/Badges.cs
@@ -2,7 +2,7 @@ namespace Styx.Api.Models
 {
     public class Badges
     {
-        public string value { get; set; }
+        public int value { get; set; }
         public List<string> imageUrls { get; set; } = new List<string>();
     }
 }
